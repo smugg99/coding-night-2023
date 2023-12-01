e917a9111cfac97b029382e9b2217b448442564c
Lis0v <kk2203768@gmail.com>
2023-12-01 23:57:24 +0100

    mapka

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -19,3 +19,31 @@
 	<button type="button" >register</button>
 
 </main>
+
+<br>
+
+<script>
+    import 'leaflet/dist/leaflet.css'; //Don't forget to declare leaflet css
+    import {Map, TileLayer, Marker, Popup} from 'svelte-map-leaflet'
+
+    const mapOptions = { center: [49.609841,20.703781], zoom: 16};
+    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+    const markerLatLng = [49.609841,20.703781];
+    const popupMessage = "Statue of Liberty National Monument";
+</script>
+
+<div class="map">
+    <Map options={mapOptions}>
+        <TileLayer url={tileUrl}></TileLayer>
+        <Marker latLng={markerLatLng}>
+            <Popup>{popupMessage}</Popup>
+        </Marker>
+    </Map>
+</div>
+
+<style>
+    .map{
+        height: 400px;
+        width: 400px;
+    }
+</style>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,6 @@
 import './build/smui.css'
 import App from './App.svelte'
+import 'leaflet/dist/leaflet.css';
 
 const app = new App({
   target: document.getElementById('app')!,
