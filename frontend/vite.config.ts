b06f182dc048a3a9f41e2700c37062a3e7165e98
Dudus Jestem <dudak799@gmail.com>
2023-12-01 22:40:29 +0100

    migrated to typescript

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import ViteSassPlugin from 'vite-plugin-sass';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), ViteSassPlugin()],
+  plugins: [svelte()],
 })
