44d0c45e325af7064c4e95e59c7df45b97261acf
Dudus Jestem <dudak799@gmail.com>
2023-12-01 23:59:50 +0100

    added map

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,23 +1,35 @@
+<!-- App.svelte -->
 <head>
 	<!-- SMUI Styles -->
 	<link rel="stylesheet" href="/build/smui.css" media="(prefers-color-scheme: light)" />
 	<link
 		rel="stylesheet"
-		href="/build/smui-dark.css"
+		href="/build/smui.css"
 		media="screen and (prefers-color-scheme: dark)"
+	/>
+
+	<!-- SMUI Styles -->
+	<!-- <link rel="stylesheet" href="node_modules/svelte-material-ui/bare.css" /> -->
+
+	<!-- Hint where we get fonts from. -->
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+
+	<!-- Material Icons, Roboto, and Roboto Mono fonts -->
+	<link
+		href="https://fonts.googleapis.com/css2?family=Material+Icons&Roboto+Mono:ital@0;1&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+		rel="stylesheet"
 	/>
 </head>
 
+<script lang="ts">
+	import Navbar from './lib/Navbar.svelte';
+</script>
+
 <main>
-	<h1>Haseus</h1>
-
-	<p>
-	Help The Others During Crises
-	</p>
-
-	<button type="button" >login</button>
-	<button type="button" >register</button>
-
+	<Navbar>
+		
+	</Navbar>
 </main>
 
 <br>
---
+++ frontend/src/lib/Navbar.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import TopAppBar, {
+		Row,
+		Section,
+		Title,
+		AutoAdjust,
+	} from '@smui/top-app-bar';
+	import IconButton from '@smui/icon-button';
+
+	let topAppBar: TopAppBar;
+</script>
+
+<nav>
+	<TopAppBar bind:this={topAppBar} variant="fixed">
+	<Row>
+		<Section>
+		<IconButton class="material-icons">menu</IconButton>
+		<Title>Haseus</Title>
+		</Section>
+		<Section align="end" toolbar>
+
+		</Section>
+	</Row>
+	</TopAppBar>
+</nav>
