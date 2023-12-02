c8ece832595ceb1e650899c36504440059983e0e
Piotr Nowakowski <piotrek02nowakowski@gmail.com>
2023-12-02 08:55:33 +0100

    fontend localization

--- frontend/src/lib/Map.svelte
+++ frontend/src/lib/Map.svelte
@@ -7,20 +7,32 @@
 
 const options = {method: 'GET', headers: {'User-Agent': 'insomnium/0.2.3'}};
 
-fetch('http://localhost:8080/incident/?lat=49.60925678752547&lon=20.697714282865814&distance=10.0', options)
-  .then(response => response.json())
-  .then(response => console.log(response))
-  .catch(err => console.error(err));
+let mapOptions = {}
+let titleUrl = ""
 
-    const mapOptions = { center: [49.60925678752547, 20.697714282865814], zoom: 16, zoomControl: false};
-    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((pos) => {
+            const [lat, lon] = [pos.coords.latitude, pos.coords.longitude]
+
+            const incidents = fetch(`http://localhost:8080/incident/?lat=${lat}&lon=${lon}&distance=10.0`, options)
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err))
+
+            mapOptions = { center: [lat, lon], zoom: 16, zoomControl: false}
+            titleUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            
+        },
+        null,
+    )
+}
     // const markerLatLng = [49.609841,20.703781];
     // const popupMessage = "Statue of Liberty National Monument";
 </script>
 
 <div class="map">
 	<Map bind:this={map} options={mapOptions}>
-		<TileLayer url={tileUrl}></TileLayer>
+		<TileLayer url={titleUrl}></TileLayer>
 		<!-- <Marker latLng={markerLatLng}>
 			<Popup>{popupMessage}</Popup>
 		</Marker> -->
